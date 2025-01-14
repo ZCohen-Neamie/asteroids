4bdabc43d022ef0fb7f225d4c577a7c86e54b8f7
zcohen-neamie <zachjocohen@gmail.com>
2025-01-14 18:07:11 -0500

    more efficient grouping system now implemented

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)   
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 
@@ -21,10 +25,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
-
         screen.fill((0,0,0))
-        player.draw(screen)
+        updatables.update(dt)
+        for sprite in drawables: 
+            sprite.draw(screen) 
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000 
