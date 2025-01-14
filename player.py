e83233cc806eea64f02b4084619a5bf0054f271a
zcohen-neamie <zachjocohen@gmail.com>
2025-01-14 17:52:16 -0500

    triangle moves with w and s

--- player.py
+++ player.py
@@ -21,10 +21,24 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt 
 
-    def update(self, dt): 
+    def move(self, dt):
+        # create the forward vector 
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        # add forward vector to player vector to move player
+        self.position += forward * PLAYER_SPEED * dt 
+
+    def update(self, dt):
         keys = pygame.key.get_pressed() 
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+   
+
+    
