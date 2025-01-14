bc8f083cf8cf3ad1612f191f05f125ddd6dbfc7c
zcohen-neamie <zachjocohen@gmail.com>
2025-01-14 17:45:06 -0500

    triangle rotates now with a and d

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
@@ -28,6 +30,6 @@ def main():
         dt = clock.tick(60) / 1000 
 
         
-        
+
 if __name__ == "__main__": 
     main() 
